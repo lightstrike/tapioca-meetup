94a00d9907c45dc68572302950bb9aef46e2d05c
Geoffrey Sechter <geoffrey.sechter@gmail.com>
2015-11-23 17:41:55 -0500

    adding rest of API endpoints

--- tapioca_meetup/resource_mapping.py
+++ tapioca_meetup/resource_mapping.py
@@ -1,25 +1,18 @@
 # coding: utf-8
 
-"""
-TODO: Add the following to mapping:
-    * /pro
-    * /photos
-    * /oembed
-    * /notifcations
-    * /status
-    * /comments
-    * v2 & v3 groups
-    * /activity
-    * v2 & v3 events
-    * /dashboard
-    * /2/cities
-    * /2/categories
-    * boards
-    * batch
-    * abuse
-"""
-
 RESOURCE_MAPPING = {
+    'api_status': {
+        'resource': '/status',
+        'docs': 'http://www.meetup.com/meetup_api/docs/status',
+    },
+    'pro_groups': {
+        'resource': '/pro/{group_url}/groups',
+        'docs': 'http://www.meetup.com/meetup_api/docs/pro/:urlname/groups',
+    },
+    'pro_members': {
+        'resource': '/pro/{group_url}/members',
+        'docs': 'http://www.meetup.com/meetup_api/docs/pro/:urlname/members',
+    },
     'venues_list': {
         'resource': '/2/venues',
         'docs': 'http://www.meetup.com/meetup_api/docs/2/venues',
@@ -51,6 +44,18 @@ RESOURCE_MAPPING = {
     'open_events': {
         'resource': '/2/open_events',
         'docs': 'http://www.meetup.com/meetup_api/docs/2/open_events/',
+    },
+    'events_list': {
+        'resource': '/2/events',
+        'docs': 'http://www.meetup.com/meetup_api/docs/2/events/',
+    },
+    'create_event': {
+        'resource': '/2/event',
+        'docs': 'http://www.meetup.com/meetup_api/docs/2/event/',
+    },
+    'event_attendance': {
+        'resource': '/{group_url}/events/{event_id}/attendance',
+        'docs': 'http://www.meetup.com/meetup_api/docs/:urlname/events/:id/attendance',
     },
     'rsvps_list': {
         'resource': '/2/rsvps',
@@ -98,10 +103,202 @@ RESOURCE_MAPPING = {
     },
     'member_approval': {
         'resource': '/{group_url}/member/approvals',
-        'docs': 'http://www.meetup.com/meetup_api/docs/:urlname/member/approvals',
+        'docs': 'http://www.meetup.com/meetup_api/docs/:urlname/member/approvals/#create',
     },
-    '': {
-        'resource': '/',
-        'docs': 'http://www.meetup.com/meetup_api/docs/',
+    'member_block_status': {
+        'resource': '/self/blocks/{member_id}',
+        'docs': 'http://www.meetup.com/meetup_api/docs/self/blocks/:member_id',
+    },
+    'report_abuse': {
+        'resource': '/self/abuse_reports',
+        'docs': 'http://www.meetup.com/meetup_api/docs/self/abuse_reports',
+    },
+    'report_group_abuse': {
+        'resource': '/{group_url}/abuse_reports',
+        'docs': 'http://www.meetup.com/meetup_api/docs//:urlname/abuse_reports',
+    },
+    'batch_request': {
+        'resource': '/batch',
+        'docs': 'http://www.meetup.com/meetup_api/docs/batch',
+    },
+    'discussion_boards': {
+        'resource': '/{group_url}/boards',
+        'docs': 'http://www.meetup.com/meetup_api/docs/:urlname/boards',
+    },
+    'discussions': {
+        'resource': '/{group_url}/boards/{board_id}/discussions',
+        'docs': 'http://www.meetup.com/meetup_api/docs/:urlname/boards/:bid/discussions',
+    },
+    'discussion_posts': {
+        'resource': '/{group_url}/boards/{board_id}/discussions/{discussion_id}',
+        'docs': 'http://www.meetup.com/meetup_api/docs/:urlname/boards/:bid/discussions/:did',
+    },
+    'categories': {
+        'resource': '/2/categories',
+        'docs': 'http://www.meetup.com/meetup_api/docs/2/categories',
+    },
+    'cities': {
+        'resource': '/2/cities',
+        'docs': 'http://www.meetup.com/meetup_api/docs/2/cities',
+    },
+    'dashboard': {
+        'resource': '/dashboard',
+        'docs': 'http://www.meetup.com/meetup_api/docs/dashboard',
+    },
+    'member_events': {
+        'resource': '/self/events',
+        'docs': 'http://www.meetup.com/meetup_api/docs/self/events',
+    },
+    'event_detail': {
+        'resource': '/{group_url}/events/{event_id}',
+        'docs': 'http://www.meetup.com/meetup_api/docs/:urlname/events/:id',
+    },
+    'event_change': {
+        'resource': '/2/events/{event_id}',
+        'docs': 'http://www.meetup.com/meetup_api/docs/2/events/:id',
+    },
+    'event_payments': {
+        'resource': '/{group_url}/events/{event_id}/payments',
+        'docs': 'http://www.meetup.com/meetup_api/docs/:urlname/events/:id/payments',
+    },
+    'event_watchlist': {
+        'resource': '/{group_url}/events/{event_id}/watchlist',
+        'docs': 'http://www.meetup.com/meetup_api/docs/:urlname/events/:id/watchlist',
+    },
+    'event_comments': {
+        'resource': '/2/event_comments',
+        'docs': 'http://www.meetup.com/meetup_api/docs/2/event_comments',
+    },
+    'create_event_comment': {
+        'resource': '/2/event_comment',
+        'docs': 'http://www.meetup.com/meetup_api/docs/2/event_comment',
+    },
+    'event_comment_detail': {
+        'resource': '/2/event_comment/{comment_id}',
+        'docs': 'http://www.meetup.com/meetup_api/docs/2/event_comment/:id',
+    },
+    'flag_event_comment': {
+        'resource': '/2/event_comment_flag',
+        'docs': 'http://www.meetup.com/meetup_api/docs/2/event_comment_flag',
+    },
+    'subscribe_to_event_comment': {
+        'resource': '/2/event_comment_subscribe/{comment_id}',
+        'docs': 'http://www.meetup.com/meetup_api/docs/2/event_comment_subscribe/:id',
+    },
+    'like_event_comment': {
+        'resource': '/2/event_comment_like/{commet_id}',
+        'docs': 'http://www.meetup.com/meetup_api/docs/2/event_comment_like/:id',
+    },
+    'event_comment_likes': {
+        'resource': '/2/event_comment_likes',
+        'docs': 'http://www.meetup.com/meetup_api/docs/2/event_comment_likes',
+    },
+    'event_ratings_list': {
+        'resource': '/2/event_ratings',
+        'docs': 'http://www.meetup.com/meetup_api/docs/2/event_ratings',
+    },
+    'event_rating_detail': {
+        'resource': '/2/event_rating',
+        'docs': 'http://www.meetup.com/meetup_api/docs/2/event_rating',
+    },
+    'concierge': {
+        'resource': '/2/concierge',
+        'docs': 'http://www.meetup.com/meetup_api/docs/2/concierge',
+    },
+    'activity_feed': {
+        'resource': '/activity',
+        'docs': 'http://www.meetup.com/meetup_api/docs/activity',
+    },
+    'find_groups': {
+        'resource': '/find/groups',
+        'docs': 'http://www.meetup.com/meetup_api/docs/find/groups',
+    },
+    'groups_list': {
+        'resource': '/2/groups',
+        'docs': 'http://www.meetup.com/meetup_api/docs/2/groups',
+    },
+    'upload_group_photo': {
+        'resource': '/2/group_photo',
+        'docs': 'http://www.meetup.com/meetup_api/docs/2/group_photo',
+    },
+    'group_detail': {
+        'resource': '/{group_url}',
+        'docs': 'http://www.meetup.com/meetup_api/docs/:urlname',
+    },
+    'group_topics': {
+        'resource': '/{group_url}/topics',
+        'docs': 'http://www.meetup.com/meetup_api/docs/:urlname/topics',
+    },
+    'recommended_groups': {
+        'resource': '/recommended/groups',
+        'docs': 'http://www.meetup.com/meetup_api/docs/recommended/groups',
+    },
+    'ignore_recommended_group': {
+        'resource': '/recommended/groups/ignores/{group_url}',
+        'docs': 'http://www.meetup.com/meetup_api/docs/recommended/groups/ignores/:urlname',
+    },
+    'find_similar_groups': {
+        'resource': '/{group_url}/similar_groups',
+        'docs': 'http://www.meetup.com/meetup_api/docs/:urlname/similar_groups',
+    },
+    'group_comments': {
+        'resource': '/comments',
+        'docs': 'http://www.meetup.com/meetup_api/docs/comments',
+    },
+    'notifications_list': {
+        'resource': '/notifications',
+        'docs': 'http://www.meetup.com/meetup_api/docs/notifications',
+    },
+    'mark_notifications_as_read': {
+        'resource': '/notifications/read',
+        'docs': 'http://www.meetup.com/meetup_api/docs/notifications/read',
+    },
+    'group_oembed': {
+        'resource': '/oembed',
+        'docs': 'http://www.meetup.com/meetup_api/docs/oembed',
+    },
+    'photo_albums_list': {
+        'resource': '/2/photo_albums',
+        'docs': 'http://www.meetup.com/meetup_api/docs/2/photo_albums',
+    },
+    'create_photo_album': {
+        'resource': '/2/photo_album',
+        'docs': 'http://www.meetup.com/meetup_api/docs/2/photo_album',
+    },
+    'photo_albums_photos_list': {
+        'resource': '/:urlname/photo_albums/:album_id/photos',
+        'docs': 'http://www.meetup.com/meetup_api/docs/:urlname/photo_albums/:album_id/photos',
+    },
+    'photo_album_detail': {
+        'resource': '/:urlname/photo_albums/:album_id',
+        'docs': 'http://www.meetup.com/meetup_api/docs/:urlname/photo_albums/:album_id',
+    },
+    'event_photos_list': {
+        'resource': '/:urlname/events/:event_id/photos',
+        'docs': 'http://www.meetup.com/meetup_api/docs/:urlname/events/:event_id/photos',
+    },
+    'event_photo_detail': {
+        'resource': '/:urlname/events/:event_id/photos/:photo_id',
+        'docs': 'http://www.meetup.com/meetup_api/docs/:urlname/events/:event_id/photos/:photo_id',
+    },
+    'photo_comments_list': {
+        'resource': '/2/photo_comments',
+        'docs': 'http://www.meetup.com/meetup_api/docs/2/photo_comments',
+    },
+    'create_photo_comment': {
+        'resource': '/2/photo_comment',
+        'docs': 'http://www.meetup.com/meetup_api/docs/2/photo_comment',
+    },
+    'delete_photo_comment': {
+        'resource': '/:urlname/events/:event_id/photos/:photo_id/comments/:comment_id',
+        'docs': 'http://www.meetup.com/meetup_api/docs/:urlname/events/:event_id/photos/:photo_id/comments/:comment_id',
+    },
+    'upload_event_photo': {
+        'resource': '/2/photo',
+        'docs': 'http://www.meetup.com/meetup_api/docs/2/photo',
+    },
+    'manage_event_photo': {
+        'resource': '/2/photo/{photo_id}',
+        'docs': 'http://www.meetup.com/meetup_api/docs/2/photo/:id',
     },
 }
--- tests/test_tapioca_meetup.py
+++ tests/test_tapioca_meetup.py
@@ -1,8 +1,10 @@
 # coding: utf-8
 
 import unittest
+import requests
 
 from tapioca_meetup import Meetup
+from tapioca_meetup.resource_mapping import RESOURCE_MAPPING
 
 
 class TestTapiocaMeetup(unittest.TestCase):
@@ -10,6 +12,23 @@ class TestTapiocaMeetup(unittest.TestCase):
     def setUp(self):
         self.wrapper = Meetup()
 
+    def test_resource_docs_exist(self):
+        """
+        Test that all docs in resource mapping
+        return a 200 HTTP code
+        """
+        for endpoint in RESOURCE_MAPPING.values():
+            response = requests.get(endpoint['docs'])
+            assert response.status_code == 200
+
+    def test_resource_docs_return_valid_information(self):
+        """
+        Testing resource docs return valid information using
+        the proxy of the resource URL
+
+        """
+        # FIXME: is there a reasonable way to check this?
+        pass
 
 if __name__ == '__main__':
     unittest.main()
